8174b82f299700bb3212cf594b477df44f8b7e9b
Samuel <chiodisamuel@gmail.com>
2025-03-16 03:02:00 -0300

    terraform changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,23 @@ variable "username" {
   description = "The username for the local account that will be created on the new VM."
   default     = "azureadmin"
 }
+
+variable "AZURE_CLIENT_ID" {
+  description = "Azure client ID for the Service Principal"
+  type        = string
+}
+
+variable "AZURE_CLIENT_SECRET" {
+  description = "Azure client secret for the Service Principal"
+  type        = string
+}
+
+variable "AZURE_TENANT_ID" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  description = "Azure subscription ID"
+  type        = string
+}
