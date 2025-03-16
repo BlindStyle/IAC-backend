156fd72eb0f2656c22aef70084fce02abe4cacca
Samuel <chiodisamuel@gmail.com>
2025-03-16 03:35:45 -0300

    minor changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -24,5 +24,9 @@ provider "azurerm" {
   client_secret   = var.AZURE_CLIENT_SECRET
   tenant_id       = var.AZURE_TENANT_ID
   subscription_id = var.AZURE_SUBSCRIPTION_ID
+
+  use_msi = false # This forces Terraform to use the Service Principal instead of Managed Identity
 }
+
+
 
